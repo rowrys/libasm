3de4a783ea02af78c4f60689431f29c53615bc65
lasere77 <colin.mewen@gmail.com>
2026-07-07 12:07:58 +0200

    perf(ft_strcmp): implement swar approach for ft_strcmp, it do not check cross page yet.

--- srcs/ft_strcmp.asm
+++ srcs/ft_strcmp.asm
@@ -1,23 +1,69 @@
 bits 64
 global ft_strcmp
 
+%define lowMask 0x0101010101010101
+%define highMask 0x8080808080808080
+
 section .text
 ;
 ;rdi -> char *s1
 ;rsi -> char *s2
 ;
+
 ft_strcmp:
-	xor ecx, ecx
-	xor eax, eax
-.loop:
-	mov al, byte [rdi + rcx]
-	cmp al, byte [rsi + rcx]
-	jne .quit
+	mov r9, lowMask
+	mov r10, highMask
+.loopAlign:
+	test rdi, 7
+	je .loopSwar
+	movzx rax, byte [rdi]
 	test al, al
-	je .quit
-	inc rcx
-	jmp .loop
-.quit:
-	movzx ecx, byte [rsi + rcx]
-	sub eax, ecx
+	je .done
+	cmp al, byte [rsi]
+	jne .done
+	inc rdi
+	inc rsi
+	jmp .loopAlign
+.loopSwar:
+	mov rdx, qword [rdi]
+	mov rax, rdx
+	not rax
+	sub rdx, r9
+	and rdx, rax
+	test rdx, r10
+	jne .loopRest
+	not rax
+	cmp rax, qword [rsi]
+	jne .loopRest
+	add rdi, 8
+	add rsi, 8
+	jmp .loopSwar
+.loopRest:
+	movzx rax, byte [rdi]
+	test al, al
+	je .done
+	cmp al, byte [rsi]
+	jne .done
+	inc rdi
+	inc rsi
+	jmp .loopRest
+.done:
+	movzx rsi, byte [rsi] 
+	sub eax, esi
 	ret
+
+; ft_strcmp:
+; 	xor ecx, ecx
+; 	xor eax, eax
+; .loop:
+; 	mov al, byte [rdi + rcx]
+; 	cmp al, byte [rsi + rcx]
+; 	jne .quit
+; 	test al, al
+; 	je .quit
+; 	inc rcx
+; 	jmp .loop
+; .quit:
+; 	movzx ecx, byte [rsi + rcx]
+; 	sub eax, ecx
+; 	ret
